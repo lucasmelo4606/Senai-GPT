5be223bffe790848ddcbc366e9fae9131b3b97a2
Lucas Melo <lucasmelo4606@gmail.com>
2025-08-22 21:53:15 -0300

    condicionais js

---
+++ javascript/condicionais.js
@@ -0,0 +1,11 @@
+    //  EXERCICIO 1
+let idade = prompt("Qual é sua idade ?")
+
+if (idade >= 18){
+    alert ("parabéns, você é maior de idade.")
+
+}
+else{
+
+    alert("voce é menor de idade ")
+} 
---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+// console.log("hello word");
+
+
+// let nome = prompt("Qual seu nome ?");
+
+// let idade = prompt("Qual a sua idade ?");
+
+// alert("meu nome é " + nome + "Tenho " + idade + "anos" )
+
+
+
